517b377e1ac87d02449874d5fa1661a5dbb3a423
paulpdoa <polopdoandres@gmail.com>
2023-08-05 22:49:18 +0800

    first commit

---
+++ server/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Port Connection
+const port = 8000;
+
+//Database Connection
+const uri = process.env.DB_URI;
+
+app.listen(port, async () => {
+    try {
+        await mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true });
+        console.log(`Listening to port ${port} and connected to database successfully`);
+    } catch(err) {
+        console.log(err);
+    }  
+})
+
+// Use
+app.use(cors());
+app.use(express.json());
