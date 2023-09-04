2ed4d7f18e59408b9fe1a90f57187d7a730ebc31
paulpdoa <polopdoandres@gmail.com>
2023-09-04 22:07:51 +0800

    added iframes for maps

--- client/src/pages/customer/CustomerRecycleCenter.jsx
+++ client/src/pages/customer/CustomerRecycleCenter.jsx
@@ -2,12 +2,6 @@ import { useState } from 'react';
 
 const CustomerRecycleCenter = () => {
 
-    const [openMap,setOpenMap] = useState(false);
-
-    const openRecycleCenter = (brgy) => {
-        console.log(brgy)
-    }
-
     return (
         <div className="h-screen w-full">
            <div className="bg-green-200 p-5">
@@ -16,8 +10,9 @@ const CustomerRecycleCenter = () => {
             {/* Working Google Maps with markers / will be used as backup */}
             {/* <iframe src="https://www.google.com/maps/d/u/0/embed?mid=19cOor4sIKOqIT25SjkumAtDr2DObtrQ&ehbc=2E312F" width="640" height="480"></iframe> */}
             {/* Working Atlist maps with markers */}
-            <iframe src="https://my.atlist.com/map/80646517-3994-498e-913c-68334abce1af?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="85%" frameborder="0" scrolling="no" allowfullscreen></iframe>
-        </div>
+            {/* <iframe src="https://my.atlist.com/map/80646517-3994-498e-913c-68334abce1af?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="85%" frameborder="0" scrolling="no" allowfullscreen></iframe> */}
+            <iframe src="https://my.atlist.com/map/bf6b6cf7-bdad-4f7d-9117-1759b5b2c7c8?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="85%" frameborder="0" scrolling="no" allowfullscreen></iframe>
+       </div>
     )
 }
 
