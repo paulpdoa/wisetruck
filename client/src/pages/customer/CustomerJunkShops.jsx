17e733b7055b7368c36fe4962922a7afdb4a85a6
paulpdoa <polopdoandres@gmail.com>
2023-09-05 09:59:36 +0800

    edit maps

--- client/src/pages/customer/CustomerJunkShops.jsx
+++ client/src/pages/customer/CustomerJunkShops.jsx
@@ -8,7 +8,7 @@ const CustomerJunkShops = () => {
             {/* <iframe src="https://www.google.com/maps/d/u/0/embed?mid=19cOor4sIKOqIT25SjkumAtDr2DObtrQ&ehbc=2E312F" width="100%" height="85%"></iframe> */}
             {/* Working Atlist maps with markers */}
             {/* <iframe src="https://my.atlist.com/map/ec46811b-0d32-4428-b442-41844df17821?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="85%" frameborder="0" scrolling="no" allowfullscreen></iframe>         */}
-            <iframe src="https://my.atlist.com/map/7c3d70a1-b36e-452d-a4a0-496b48efa66f?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="85%" frameborder="0" scrolling="no" allowfullscreen></iframe>
+            <iframe src="https://my.atlist.com/map/7c3d70a1-b36e-452d-a4a0-496b48efa66f?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" frameborder="0" scrolling="no" allowfullscreen></iframe>
         </div>
     )
 }
--- client/src/pages/customer/CustomerRecycleCenter.jsx
+++ client/src/pages/customer/CustomerRecycleCenter.jsx
@@ -11,7 +11,7 @@ const CustomerRecycleCenter = () => {
             {/* <iframe src="https://www.google.com/maps/d/u/0/embed?mid=19cOor4sIKOqIT25SjkumAtDr2DObtrQ&ehbc=2E312F" width="640" height="480"></iframe> */}
             {/* Working Atlist maps with markers */}
             {/* <iframe src="https://my.atlist.com/map/80646517-3994-498e-913c-68334abce1af?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="85%" frameborder="0" scrolling="no" allowfullscreen></iframe> */}
-            <iframe src="https://my.atlist.com/map/bf6b6cf7-bdad-4f7d-9117-1759b5b2c7c8?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="85%" frameborder="0" scrolling="no" allowfullscreen></iframe>
+            <iframe src="https://my.atlist.com/map/bf6b6cf7-bdad-4f7d-9117-1759b5b2c7c8?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" frameborder="0" scrolling="no" allowfullscreen></iframe>
        </div>
     )
 }
