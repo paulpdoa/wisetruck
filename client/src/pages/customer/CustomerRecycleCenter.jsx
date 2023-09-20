a595863ded0eab556a76c64d960e1bde4385b72f
paulpdoa <polopdoandres@gmail.com>
2023-09-20 13:52:24 +0800

    updated maps, expired atlist

--- client/src/pages/customer/CustomerRecycleCenter.jsx
+++ client/src/pages/customer/CustomerRecycleCenter.jsx
@@ -11,7 +11,8 @@ const CustomerRecycleCenter = () => {
             {/* <iframe src="https://www.google.com/maps/d/u/0/embed?mid=19cOor4sIKOqIT25SjkumAtDr2DObtrQ&ehbc=2E312F" width="640" height="480"></iframe> */}
             {/* Working Atlist maps with markers */}
             {/* <iframe src="https://my.atlist.com/map/80646517-3994-498e-913c-68334abce1af?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="85%" frameborder="0" scrolling="no" allowfullscreen></iframe> */}
-            <iframe src="https://my.atlist.com/map/bf6b6cf7-bdad-4f7d-9117-1759b5b2c7c8?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" frameborder="0" scrolling="no" allowfullscreen></iframe>
+            {/* <iframe src="https://my.atlist.com/map/bf6b6cf7-bdad-4f7d-9117-1759b5b2c7c8?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" frameborder="0" scrolling="no" allowfullscreen></iframe> */}
+            <iframe src="https://my.atlist.com/map/d81dd3d4-33e8-4e41-85d2-f3a6fbf00fab?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" frameborder="0" scrolling="no" allowfullscreen></iframe>
        </div>
     )
 }
