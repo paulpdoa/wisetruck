e00275291afb7f7712294e8ca0cb5bbcc694c897
rizzgon <ranilorigon.0424@gmail.com>
2023-11-13 11:55:34 +0800

    inupdate yung junkshop at recycle centers map

--- client/src/pages/customer/CustomerRecycleCenter.jsx
+++ client/src/pages/customer/CustomerRecycleCenter.jsx
@@ -14,7 +14,7 @@ const CustomerRecycleCenter = () => {
             {/* <iframe src="https://my.atlist.com/map/80646517-3994-498e-913c-68334abce1af?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="85%" frameborder="0" scrolling="no" allowfullscreen></iframe> */}
             {/* <iframe src="https://my.atlist.com/map/bf6b6cf7-bdad-4f7d-9117-1759b5b2c7c8?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" frameborder="0" scrolling="no" allowfullscreen></iframe> */}
             {/* <iframe src="https://my.atlist.com/map/d81dd3d4-33e8-4e41-85d2-f3a6fbf00fab?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" frameborder="0" scrolling="no" allowfullscreen></iframe> */}
-            <iframe src="https://my.atlist.com/map/e0a1f609-caa0-4ca8-a901-0683ff6ffff7?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" frameborder="0" scrolling="no" allowfullscreen></iframe>
+            <iframe src="https://my.atlist.com/map/0d4b4b5a-f9d5-4b2e-9586-0a041067a127?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" frameborder="0" scrolling="no" allowfullscreen></iframe>
        </div>
     )
 }
