1fd939bb9b7a05657b597caab3cdbea7baa5616f
rizzgon <ranilorigon.0424@gmail.com>
2024-01-21 21:43:16 +0800

    map changes

--- client/src/pages/customer/CustomerJunkShops.jsx
+++ client/src/pages/customer/CustomerJunkShops.jsx
@@ -11,7 +11,7 @@ const CustomerJunkShops = () => {
             {/* <iframe src="https://my.atlist.com/map/ec46811b-0d32-4428-b442-41844df17821?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="85%" frameborder="0" scrolling="no" allowfullscreen></iframe>         */}
             {/* <iframe src="https://my.atlist.com/map/7c3d70a1-b36e-452d-a4a0-496b48efa66f?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" frameborder="0" scrolling="no" allowfullscreen></iframe> */}
             {/* <iframe src="https://my.atlist.com/map/3212a212-de25-410b-be85-5c546ccf3cd2?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" frameborder="0" scrolling="no" allowfullscreen></iframe> */}
-            <iframe src="https://my.atlist.com/map/64606ec5-9499-4f10-b1e1-43502f4fb0d6?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" loading="lazy" frameborder="0" scrolling="no" allowfullscreen></iframe>
+            <iframe src="https://my.atlist.com/map/df84daa7-5443-4bcc-acad-f508a2392f85?share=true" allow="geolocation 'self' https://my.atlist.com" width="100%" height="700px" loading="lazy" frameborder="0" scrolling="no" allowfullscreen></iframe>
         </div>
     )
 }
